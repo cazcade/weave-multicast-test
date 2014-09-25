6ec9ac9092904ac6f017bcf5cce99e97bd917d67
neilellis <neilellis@cazcade.com>
2014-09-25 22:01:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
+curl http://www.nmsl.cs.ucsb.edu/MulticastSocketsBook/c_send_receive.tar.gz | tar xzvf -
+gcc -o mcsend c/mcsend.c ; gcc -o mcreceive c/mcreceive.c
 while ! grep -q ^1$ /sys/class/net/ethwe/carrier 2>/dev/null
 do sleep 1
 done
@@ -6,6 +8,12 @@ ip=$(ip addr show ethwe | grep inet | grep -v inet6 | sed 's/^[ ]*inet //g'| cut
 if [[ $1 == "receive" ]] 
 then
   java -Djava.net.preferIPv4Stack=true -cp /jgroups.jar org.jgroups.tests.McastReceiverTest -bind_addr ${ip} -mcast_addr 231.12.21.132 -port 45566 
-else
+elsif [[ $1 == "send" ]] 
   java -Djava.net.preferIPv4Stack=true -cp /jgroups.jar org.jgroups.tests.McastSenderTest -bind_addr ${ip} -mcast_addr 231.12.21.132 -port 45566
+elsif [[ $1 == "rcv" ]] 
+./mcsend 239.1.2.3 1234
+elsif [[ $1 == "snd" ]] 
+./mcreceive 239.1.2.3 1234
 fi
+
+
