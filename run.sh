b8801e757e85aaf66fdea31e1cde9cda9207ab91
neilellis <neilellis@cazcade.com>
2014-09-25 16:11:08 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eu
-while ! ifconfig ethwe
-do
-    sleep 10
-    ifconfig
+while ! grep -q ^1$ /sys/class/net/ethwe/carrier 2>/dev/null
+do sleep 1
 done
 java -cp /jgroups.jar org.jgroups.tests.McastReceiverTest -mcast_addr 231.12.21.132 -port 45566 &
 java -cp /jgroups.jar org.jgroups.tests.McastSenderTest -mcast_addr 231.12.21.132 -port 45566
